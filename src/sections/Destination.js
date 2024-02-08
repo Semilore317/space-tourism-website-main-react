d04cc97bd907fea140eff89f52c46aba976cef27
Semilore317 <semi.banks.317@gmail.com>
2024-02-08 10:44:36 +0100

    having issues for now

--- src/sections/Destination.js
+++ src/sections/Destination.js
@@ -1,13 +1,50 @@
-import React from 'react'
+import React from "react";
+import moon from "./destination assets/image-moon.png";
+import data from "./../data";
 
 function Destination() {
+  const [planetNumber, setPlanetNumber] = React.useState(2);
   return (
-    <div className='destination'>
-        <div className="destination-content">
-          
-        </div>
+    <div className="destination">
+      <div className="destination-content">
+        {data.map((planet) => {
+          console.log(planet)
+          return (
+            <>
+              <h1 className="destination-content-subtitle">
+                <span>01</span> pick your destination
+              </h1>
+              <div className="destination-content-image">
+                <img src={moon} alt="" />
+              </div>
+              <div className="destination-content-navigation">
+                <div className="tab">Moon</div>
+                <div className="tab">Mars</div>
+                <div className="tab">Europa</div>
+                <div className="tab">Titan</div>
+              </div>
+              <h1 className="destination-content-title">
+                {data.destinations[planetNumber].name}
+              </h1>
+              <p className="destination-content-body">
+                {data.destinations[planetNumber].description}
+              </p>
+              <div className="destination-content-info">
+                <div className="distance">
+                  <p>avg. distance</p>
+                  <h1>384400km</h1>
+                </div>
+                <div className="time">
+                  <p>est. travel time</p>
+                  <h1>3 days</h1>
+                </div>
+              </div>
+            </>
+          );
+        })}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Destination
+export default Destination;
