c907e1bb629cf45aaacf20a3f8e314c6166a9f1b
Semilore317 <semi.banks.317@gmail.com>
2024-02-08 09:39:33 +0100

    done with home page ;)

--- src/sections/Destination.js
+++ src/sections/Destination.js
@@ -3,7 +3,9 @@ import React from 'react'
 function Destination() {
   return (
     <div className='destination'>
-        
+        <div className="destination-content">
+          
+        </div>
     </div>
   )
 }
--- src/sections/Home.js
+++ src/sections/Home.js
@@ -1,17 +1,19 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
     <div className='home'>
         <div className="home-content">
           <div className="home-content-subtitle">
-            So, you want to travel to space
+            So, you want to travel to 
           </div>
-          <h1 className="home-content-title">Space</h1>
+          <h1 className="home-content-title"><h3>Space</h3></h1>
           <p className='home-content-body'>Let's face it; if you want to go to space, you might as well genuinely go to outer space and not hover over the edge of it. Well sit back and relax becuase we'll give you a truly out of this World experience!
           </p>
           <div className="home-content-explore">
-            Explore
+            <Link to='/destination'><h3>Explore</h3></Link>
+            
           </div>
         </div>
     </div>
