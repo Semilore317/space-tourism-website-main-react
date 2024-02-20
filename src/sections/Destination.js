28bd4478df5dca1e565f1fa46e1e28bfd4477b77
Semilore317 <semi.banks.317@gmail.com>
2024-02-20 22:46:04 +0100

    damn.

--- src/sections/Destination.js
+++ src/sections/Destination.js
@@ -4,7 +4,7 @@ import data from "./../data";
 //ivory coast beats Nigeria
 //went out and exhausted af
 //...... .. ..installed myschool
-//hey damn
+//hey damn ...
 //fukkkkkk
 
 function Destination() {
